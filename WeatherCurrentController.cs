677b8533d122983dc52da550c315659a22c80e0c
pandzom <117646293+pandzom@users.noreply.github.com>
2023-05-21 22:07:39 +0200

    Add files via upload

---
+++ WeatherCurrentController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("weather/[controller]")]
+    [ApiController]
+    public class currentController : ControllerBase
+    {
+
+        private static readonly string[] Summaries = new[]
+       {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private static readonly string[] Location = new[]
+      {
+        "London", "Paris", "New York", "Los Angeles", "Berlin", "Sarajevo", "Beograd", "Zagreb", "Madrid", "Rome"
+    };
+
+
+
+        private readonly ILogger<currentController> _logger;
+
+        public currentController(ILogger<currentController> logger)
+        {
+            _logger = logger;
+        }
+
+       
+        // GET api/WeatherCurrentController/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+       
+    }
+}
---
+++ WeatherHistoryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("weather/[controller]")]
+    [ApiController]
+    public class historyController : ControllerBase
+    {
+
+        private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+
+        private readonly ILogger<historyController> _logger;
+
+        public historyController(ILogger<historyController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        // GET api/WeatherHistoryController/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+       
+    }
+}
